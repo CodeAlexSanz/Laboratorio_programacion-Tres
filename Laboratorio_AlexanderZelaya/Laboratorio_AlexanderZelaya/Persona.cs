b32caa5dd0fffe992bf7180f48c82abd51cfd540
CodeAlexSanz <116242323+DevAlexSanzJr@users.noreply.github.com>
2023-02-22 13:35:40 -0600

    commit

---
+++ Laboratorio_AlexanderZelaya/Laboratorio_AlexanderZelaya/Persona.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_AlexanderZelaya
+{
+    public class Persona
+    {
+        private string nombre;
+        private int edad;
+        private Departamento departamento;
+        private Municipio municipio;
+
+        public string Nombre
+        {
+            get => nombre;
+            set => nombre = value;
+        }
+
+        public int Edad
+        {
+            get => edad;
+            set => edad = value;
+        }
+
+        public Departamento Departamento
+        {
+            get => departamento;
+            set => departamento = value;
+        }
+        public Municipio Municipio
+        {
+            get => municipio;
+            set => municipio = value;
+        }
+
+        public void obtenerDatosUsuario()
+        {
+
+        }
+    }
+}
